c7f30440aa67918c9c1ec1554d8c878e200b25bb
Sebastian K <sebastian76567@gmail.com>
2018-05-28 21:05:58 +0200

    Funktionsfähigkei abgeschlossen

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -4,9 +4,46 @@ namespace HangMan
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            bool restart = true;
+            do
+            {
+                ScreenOptions.Buchstabe.Clear();
+                ScreenOptions.Used.Clear();
+                Start(ref restart);
+            }
+            while (restart);
+
+
+            Console.ReadKey();
         }
+
+
+        public static void Start(ref bool restart)
+        {
+            bool gewonnen = false;
+
+            Console.WriteLine("Enter you word");
+
+            ScreenOptions.read();
+
+            while (!gewonnen)
+            {
+                ScreenOptions.check(ref gewonnen);
+            }
+            
+
+            Console.WriteLine("Again?(y/n)");
+            ConsoleKeyInfo keyinfo = Console.ReadKey();
+            Console.WriteLine();
+
+            if (keyinfo.Key != ConsoleKey.Y)
+                restart = false;
+
+        }
+
+
     }
 }
