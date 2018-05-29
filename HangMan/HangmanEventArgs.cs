6d4e00cbb5bc897a9c2ae869305dac5c6cd7e81d
Sebastian K <sebastian76567@gmail.com>
2018-05-29 09:18:40 +0200

    Added missing files

---
+++ HangMan/HangmanEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HangMan
+{
+    class HangmanEventArgs: EventArgs
+    {
+        public int index { get; set; }
+
+        public HangmanEventArgs(int index) => this.index = index;
+
+
+    }
+}
